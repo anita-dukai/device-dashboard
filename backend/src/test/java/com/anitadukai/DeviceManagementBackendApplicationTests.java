97e70606595659b3700cc6d79a9c361e99d2f17b
Anita Dukai <dukaianita.90@gmail.com>
2025-07-06 12:36:59 +0200

    Add backend and frontend projects

---
+++ backend/src/test/java/com/anitadukai/DeviceManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anitadukai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
